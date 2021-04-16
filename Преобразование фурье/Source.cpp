ef25a72f636122f1d688ebbc4a603a06e115fa1a
Vladislav Nesterov <nesterovvlad100@gmail.com>
2021-04-17 00:44:31 +0300

    add pervie 5 elementov Funkcii Lagera

--- Преобразование фурье/Source.cpp
+++ Преобразование фурье/Source.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 const int n = 100;
-double w[n];
-int a = 6, b = 15;
+double w[n], l[5];
+int a = 6, b = 15, alfa = 2*a;
 
 
 
@@ -12,14 +13,18 @@ void wt() {
 
 	for (int i = 0; i < n; i++)
 	{
-		w[i] = exp(-a * i) * sin(b * i);
+		w[i] = (exp(-a * i) / (b - a)) + (sin(-b * i) / (a - b));
 	}
 
 
 }
-
-
-
+void L() {
+	l[0] = sqrt(2 * alfa) * exp(-alfa * 0);
+	l[1] = sqrt(2 * alfa) * exp(-alfa * 1) * (1.0 - 2.0 * alfa * 1);
+	l[2] = sqrt(2 * alfa) * exp(-alfa * 2) * (1.0 - 4 * alfa * 2 + 2 * pow(alfa,2) * pow(2,2));
+	l[3] = sqrt(2 * alfa) * exp(-alfa * 3) * (1.0 - 6 * alfa * 3 + 6 * pow(alfa, 2) * pow(3, 2) - 4 * pow(alfa, 3) * pow(3, 3) / 3);
+	l[4] = sqrt(2 * alfa) * exp(-alfa * 4) * (1.0 - 8 * alfa * 4 + 12 * pow(alfa, 2) * pow(4, 2) - 16 * pow(alfa, 3) * pow(4, 3) / 3 + 2 * pow(alfa, 4) * pow(4, 4) / 3);
+}
 
 int main() {
 
