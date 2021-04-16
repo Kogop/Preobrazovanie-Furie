36c370647d404ee78580a3076a43df741bc18fc7
Vladislav Nesterov <nesterovvlad100@gmail.com>
2021-04-17 00:01:55 +0300

    wt

--- Преобразование фурье/Source.cpp
+++ Преобразование фурье/Source.cpp
@@ -2,15 +2,34 @@
 
 using namespace std;
 
+const int n = 100;
+double w[n];
+int a = 6, b = 15;
 
 
+
+void wt() {
+
+	for (int i = 0; i < n; i++)
+	{
+		w[i] = exp(-a * i) * sin(b * i);
+	}
+
+
+}
 
 
 
 
 int main() {
 
+	wt();
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << w[i] << endl;
+	}
+	
 
 
 
